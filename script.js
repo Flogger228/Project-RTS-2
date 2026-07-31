01b272c589e038cf9655ee12fcc26478f5ed6b42
Flogger228 <yriksh8@gmail.com>
2026-07-31 19:23:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -70,16 +70,7 @@ const codeImages = [
 // ГАЛЕРЕЯ
 // ======================
 
-const gallery = [
-    "images/gallery1.jpg",
-    "images/gallery2.jpg",
-    "images/gallery3.jpg",
-    "images/gallery4.jpg",
-    "images/gallery5.jpg",
-    "images/gallery6.jpg",
-    "images/gallery7.jpg",
-    "images/gallery8.jpg"
-];
+
 
 // ======================
 // О САЙТЕ
